d455559e5bc8b9efffddd7774f14cf4d7b0c165c
Marsel Sultanov <marselsultanov777@gmail.com>
2023-02-13 08:43:15 +0400

    Create gitlab.sh

---
+++ gitlab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo yum install -y ansible-core
+ansible-galaxy collection install ansible.posix && ansible-galaxy collection install community.general
+git clone https://github.com/marselsultanov/reclaim-oled-volume.git
+ansible-playbook reclaim-oled-volume/playbook.yml -i localhost, --connection=local
